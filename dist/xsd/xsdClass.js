5998d81fba6e09b65f09bf9055f7f25b5c2dfa7c
Baudin <ckelkboom@protonmail.com>
2019-04-19 13:06:49 +0200

    Working on stuff

--- dist/xsd/xsdClass.js
+++ dist/xsd/xsdClass.js
@@ -18,9 +18,9 @@ class XsdClass {
         })).join("\n");
         return `
     <xsd:complexType name="${this.node.id}">
-        <xsd:sequence>
+        <xsd:all>
         ${fields}
-        </xsd:sequence>
+        </xsd:all>
     </xsd:complexType>
         `;
     }
@@ -63,7 +63,19 @@ class XsdClass {
             })
                 .join("\n");
             let xsdType = helpers_1.baseTypeToXSDType(fieldType);
-            if (xsdType.startsWith("xsd:")) {
+            if (isList) {
+                return `
+    <xsd:element name="${this.node.id}_${tf.id}" nillable="false">
+        <xsd:annotation>${annotations}</xsd:annotation>
+        <xsd:complexType>
+        <xsd:sequence>
+        <xsd:element ref="self:${fieldType}" minOccurs="0" maxOccurs="unbound" />
+        </xsd:sequence>
+        </xsd:complexType>
+    </xsd:element>
+            `.trim();
+            }
+            else if (xsdType.startsWith("xsd:")) {
                 return `
     <xsd:element name="${this.node.id}_${tf.id}" nillable="false">
         <xsd:annotation>${annotations}</xsd:annotation>
@@ -72,6 +84,13 @@ class XsdClass {
             ${restrictions}
         </xsd:restriction>
         </xsd:simpleType>
+    </xsd:element>
+            `.trim();
+            }
+            else if (xsdType.startsWith("self")) {
+                return `
+    <xsd:element name="${this.node.id}_${tf.id}" type="${xsdType}" nillable="false">
+        <xsd:annotation>${annotations}</xsd:annotation>
     </xsd:element>
             `.trim();
             }
